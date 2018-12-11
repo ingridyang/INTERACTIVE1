4fb8de65ef0b6685fedd25315353031b3d37f920
ingrid <ingrid.yang@cca.edu>
2018-12-11 14:40:22 -0800

    upload

--- PROJECT 3/Exercise 4/js/scroll.js
+++ PROJECT 3/Exercise 4/js/scroll.js
@@ -19,8 +19,10 @@ $(document).ready(function() {
 
     if (windowpos >= winheight) {
       $('.chatwindow').addClass('showchats');
+      $('.blocker').addClass('showblocker');
     } else if (windowpos < winheight) {
       $('.chatwindow').removeClass('showchats');
+      $('.blocker').addClass('showblocker');
     }
 
     if (windowpos >= 1100) {
@@ -111,6 +113,102 @@ $(document).ready(function() {
       $('.alert14').addClass('showalert');
     } else if (windowpos < 3300) {
       $('.alert14').removeClass('showalert');
+    }
+
+    if (windowpos >= 3500) {
+      $('.alert15').addClass('showalert');
+    } else if (windowpos < 3500) {
+      $('.alert15').removeClass('showalert');
+    }
+
+    if (windowpos >= 3700) {
+      $('.alert16').addClass('showalert');
+    } else if (windowpos < 3700) {
+      $('.alert16').removeClass('showalert');
+    }
+
+    if (windowpos >= 3800) {
+      $('.alert17').addClass('showalert');
+    } else if (windowpos < 3800) {
+      $('.alert17').removeClass('showalert');
+    }
+
+    if (windowpos >= 3900) {
+      $('.alert18').addClass('showalert');
+    } else if (windowpos < 3900) {
+      $('.alert18').removeClass('showalert');
+    }
+
+    if (windowpos >= 4000) {
+      $('.alert19').addClass('showalert');
+    } else if (windowpos < 4000) {
+      $('.alert19').removeClass('showalert');
+    }
+
+    if (windowpos >= 4200) {
+      $('.alert20').addClass('showalert');
+    } else if (windowpos < 4200) {
+      $('.alert20').removeClass('showalert');
+    }
+
+    if (windowpos >= 4400) {
+      $('.alert21').addClass('showalert');
+    } else if (windowpos < 4400) {
+      $('.alert21').removeClass('showalert');
+    }
+
+    if (windowpos >= 4600) {
+      $('.alert22').addClass('showalert');
+    } else if (windowpos < 4600) {
+      $('.alert22').removeClass('showalert');
+    }
+
+    if (windowpos >= 4800) {
+      $('.alert23').addClass('showalert');
+    } else if (windowpos < 4800) {
+      $('.alert23').removeClass('showalert');
+    }
+
+    if (windowpos >= 4900) {
+      $('.alert24').addClass('showalert');
+    } else if (windowpos < 4900) {
+      $('.alert24').removeClass('showalert');
+    }
+
+    if (windowpos >= 5000) {
+      $('.alert25').addClass('showalert');
+    } else if (windowpos < 5000) {
+      $('.alert25').removeClass('showalert');
+    }
+
+    if (windowpos >= 5100) {
+      $('.alert26').addClass('showalert');
+    } else if (windowpos < 5100) {
+      $('.alert26').removeClass('showalert');
+    }
+
+    if (windowpos >= 5200) {
+      $('.alert27').addClass('showalert');
+    } else if (windowpos < 5200) {
+      $('.alert27').removeClass('showalert');
+    }
+
+    if (windowpos >= 5300) {
+      $('.alert28').addClass('showalert');
+    } else if (windowpos < 5300) {
+      $('.alert28').removeClass('showalert');
+    }
+
+    if (windowpos >= 5500) {
+      $('.alert29').addClass('showalert');
+    } else if (windowpos < 5500) {
+      $('.alert29').removeClass('showalert');
+    }
+
+    if (windowpos >= 3300) {
+      $('.alert30').addClass('showalert');
+    } else if (windowpos < 3300) {
+      $('.alert30').removeClass('showalert');
     }
   console.log(windowpos)
 
